debe8c47642f5a7592b4ad5fab4666825a980b79
awadheshrawat <awadhesh31dec@outlook.com>
2016-10-29 00:34:34 +0530

    Arithmetic expressions

---
+++ shell/basic/shell4.sh
@@ -0,0 +1,12 @@
+# 1. Arithmetic expressions
+$ let n=52-1
+$ echo $n
+51
+
+# 2. Referencing the variables
+PRICE_PER_APPLE=5
+echo "The price of an Apple today is: \$HK $PRICE_PER_APPLE"
+
+#or
+MyFirstLetters=ABC
+echo "The first 10 letters in the alphabet are: ${MyFirstLetters}DEFGHIJ"
