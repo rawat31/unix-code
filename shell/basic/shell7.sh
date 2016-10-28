77f0aaf46c311d230abf323adaea1320baf87530
awadheshrawat <awadhesh31dec@outlook.com>
2016-10-29 00:43:17 +0530

    Arrays in shell

---
+++ shell/basic/shell7.sh
@@ -0,0 +1,27 @@
+#Basic Operators
+A=3
+B=$((100 * $A + 5)) # 305
+
+#The basic operators are:
+
+#a + b addition (a plus b)
+
+#a - b substraction (a minus b)
+
+#a * b multiplication (a times b)
+
+#a / b division (integer) (a divided by b)
+
+#a % b modulo (the integer remainder of a divided by b)
+
+#a ** b exponentiation (a to the power of b)
+
+#other Example
+
+#!/bin/bash
+COST_PINEAPPLE=50
+COST_BANANA=4
+COST_WATERMELON=23
+COST_BASKET=1
+TOTAL=78
+echo "Total Cost is $TOTAL"
